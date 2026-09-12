4dcd5385cb5aeccb7b9a279683650ce6d9d1a5ba
Rahat Karim <rahatkarim5452@gmail.com>
2026-09-12 18:36:32 +0600

    Navbar Modified

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
 import Logo from "../assets/logo-text.png";
-import { Color_Gradient } from "../colors/color";
 import { Menu, X } from "lucide-react";
 
 const Navbar = () => {
@@ -18,20 +17,18 @@ const Navbar = () => {
         <img src={Logo} alt="Logo" className="h-8 w-auto" />
 
         <ul className="hidden md:flex items-center gap-8 font-semibold text-gray-500">
-          <li className="cursor-pointer">Home</li>
-          <li className="cursor-pointer">Technologies</li>
-          <li className="cursor-pointer">Projects</li>
-          <li className="cursor-pointer">About</li>
-          <li className="cursor-pointer">Contact</li>
+          <li className="cursor-pointer hover:text-gray-900">Home</li>
+          <li className="cursor-pointer hover:text-gray-900">Technologies</li>
+          <li className="cursor-pointer hover:text-gray-900">Projects</li>
+          <li className="cursor-pointer hover:text-gray-900">About</li>
+          <li className="cursor-pointer hover:text-gray-900">Contact</li>
         </ul>
 
         <div className="flex items-center gap-2">
-          <button className="font-semibold text-gray-700 border border-gray-300 rounded-3xl px-3 py-1.5 sm:px-4 sm:py-2 text-sm sm:text-base">
+          <button className="font-semibold cursor-pointer text-gray-700 border border-gray-300 rounded-3xl px-3 py-1.5 sm:px-4 sm:py-2 text-sm sm:text-base">
             Sign In
           </button>
-          <button
-            className={`${Color_Gradient} text-white font-semibold rounded-3xl px-3 py-1.5 sm:px-5 sm:py-2 text-sm sm:text-base`}
-          >
+          <button className="bg-[#D91B7E] text-white cursor-pointer font-semibold rounded-3xl px-3 py-1.5 sm:px-5 sm:py-2 text-sm sm:text-base">
             Sign Up
           </button>
         </div>
